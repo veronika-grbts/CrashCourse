b1c11b616ca38537976fc8432c608850321e5cc5
veronika-grbts <veranikadnepr@gmail.com>
2024-10-31 17:49:19 +0200

    modified task1

--- script.rb
+++ script.rb
@@ -15,31 +15,33 @@ class Student
   def validate_date_of_birth(date_of_birth)
     date = Date.parse(date_of_birth)
     if date >= Date.today
-      puts "Date of birth must be in the past: #{surname} #{name} #{date_of_birth}"
       return nil
     end
     date
   end
 
-  def calculate_age
-    today = Date.today
-    age = today.year - @date_of_birth.year
-    age -= 1 if today < @date_of_birth + age.years
-    age
-  end
-
   def add_student(student)
     if @@students.any? { |s| s.surname == student.surname && s.name == student.name && s.date_of_birth == student.date_of_birth }
-      puts "Student #{student.surname} #{student.name} #{student.date_of_birth} wasn`t created because it is already in the list."
+      false
     else
       @@students << student
+      true
     end
   end
 
-  def remove_student
-    @@students.delete_if { |s| s.surname == @surname && s.name == @name && s.date_of_birth == @date_of_birth }
+  def self.remove_student(surname, name, date_of_birth)
+    date_of_birth = Date.parse(date_of_birth) if date_of_birth.is_a?(String)
+    students_to_remove = @@students.select { |s| s.surname == surname && s.name == name && s.date_of_birth == date_of_birth }
+    
+    if students_to_remove.any?
+      @@students.delete_if { |s| s.surname == surname && s.name == name && s.date_of_birth == date_of_birth }
+      true 
+    else
+      false 
+    end
   end
-
+  
+  
   def self.get_students_by_age(age)
     @@students.select { |student| student.calculate_age == age }
   end
@@ -56,7 +58,7 @@ class Student
     today = Date.today
     age = today.year - @date_of_birth.year
     if today.month < @date_of_birth.month || (today.month == @date_of_birth.month && today.day < @date_of_birth.day)
-      age -= 1
+      age = age - 1
     end
     
     age
@@ -85,7 +87,8 @@ puts "Students name John"
 students_name_John = Student.get_students_by_name("John")
 students_name_John.each { |student| puts student }
 
-puts "Remove studen 4"
-student4.remove_student
+puts "Remove student"
+Student.remove_student("Smith", "Anna", "1998-08-22")
+ 
 puts "List all students after remove"
 Student.all_students.each { |student| puts student }
